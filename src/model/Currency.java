cf304af7a96ff6962ede10a620b21eb88037a585
usuario <usuario@10.22.143.36>
2014-10-29 11:59:29 +0000

    Currency class completed.

--- src/model/Currency.java
+++ src/model/Currency.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Currency {
+    private String name;
+    private String symbol;
+    private int code;
     
+    public Currency (String name, String symbol, int code) {
+        this.name = name;
+        this.symbol = symbol;
+        this.code = code;
+    }
 }
