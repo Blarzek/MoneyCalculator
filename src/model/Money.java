dbdd3a25538860e81785f604636a61ab526c6c98
usuario <usuario@10.22.143.36>
2014-10-29 12:29:05 +0000

    Money class completed.

--- src/model/Money.java
+++ src/model/Money.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Money {
-    
+    private final float amount;
+    private final Currency currency;
+
+    public Money(float amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
 }
