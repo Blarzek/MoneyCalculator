4fc4fcf3f23303837104f034360271296b732e71
usuario <usuario@10.22.143.36>
2014-10-29 12:24:10 +0000

    Currency code changed to String, and CurrencySet class completed.

--- src/model/CurrencySet.java
+++ src/model/CurrencySet.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.util.HashMap;
+
 public class CurrencySet {
+    private HashMap<String,Currency> currencySet;
     
+    public CurrencySet(){
+        currencySet = new HashMap<>();
+    }
+    public Currency getCurrency(String code){
+        return currencySet.get(code);
+    }
 }
