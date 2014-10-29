5d9d645983be1f0c2944a4358032133d699cc6b2
usuario <usuario@10.22.143.36>
2014-10-29 12:25:03 +0000

    Currency code changed to String, and CurrencySet class completed.

--- src/model/Currency.java
+++ src/model/Currency.java
@@ -1,11 +1,11 @@
 package model;
 
 public class Currency {
-    private String name;
-    private String symbol;
-    private int code;
+    private final String name;
+    private final String symbol;
+    private final String code;
     
-    public Currency (String name, String symbol, int code) {
+    public Currency (String name, String symbol, String code) {
         this.name = name;
         this.symbol = symbol;
         this.code = code;
